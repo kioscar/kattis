d384ef28c29082c4e3d6b34a8a530d8314357b70
kioscar <kioscar2@gmail.com>
2017-08-27 18:57:13 -0500

    Create apaxian.java

---
+++ apaxian.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class apaxian {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String cadena1=null, cadena2="";
+        
+        cadena1=sc.nextLine();
+        //cadena2+=cadena1.charAt(0);
+        for (int i = 0; i < cadena1.length()-1; i++) {      
+            if (cadena1.charAt(i)==cadena1.charAt(i+1)) {
+                
+            }else{
+                cadena2+=cadena1.charAt(i);
+            }
+        }
+        cadena2+=cadena1.charAt(cadena1.length()-1);
+        System.out.println(cadena2);
+    }
+}
